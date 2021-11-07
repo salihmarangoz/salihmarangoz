36bcdc174b5fc8c78af0bd459c7e6e8dcb7925a6
salihmarangoz <salih285@gmail.com>
2021-11-07 13:01:41 +0100

    updated

--- .github/run.py
+++ .github/run.py
@@ -1,9 +1,24 @@
 
 import time
+from ghapi.all import GhApi
+import os
+
 
 f = open(".github/TEMPLATE.md", "r")
 readme = f.readlines()
 
+
+# https://ghapi.fast.ai/fullapi.html
+github_token = os.environ['GITHUB_TOKEN']
+api = GhApi(token=github_token)
+#github_user = api.users.get_authenticated() # github_user["login"]
+
+repos = []
+out = api.repos.list_for_authenticated_user(per_page=100, sort="pushed", type="owner")
+for o in out:
+    if not o["fork"]:
+        repos.append(o)
+        print(o["topics"], o["description"])
 
 readme.append("\n\n\n\n\n")
 readme.append("Last updated: " + time.ctime(time.time()) + "\n")
